81d1db4bbbb691ffdedb01d5a7bcaf83096459fe
Rashid Goulebe <rashid.goulebe@alumni.fh-aachen.de>
2024-10-14 17:24:18 +0200

    Update 4 Semi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Neuronale Netze zur Erkennung von&nbsp;
             <span className="inline-block relative">
-              Brainwave{" "}
+              Anomalien{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -34,14 +34,14 @@ const Hero = () => {
                 alt="Curve"
               />
             </span>
+            {` `} in Zeitreihen
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+            Die Erkennung von Anomalien in Zeitreihen dient zur Interpretation
+            und Fehlerfindung von Außreißern. Diese Seite gibt einen Überblick
+            über gängige Verfahren der Anomalieerkennung mit Neuronalen Netzen
+            und vergleicht diese.
           </p>
-          <Button href="/pricing" white>
-            Get started
-          </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
@@ -49,16 +49,6 @@ const Hero = () => {
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
 
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
-                <img
-                  src={robot}
-                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
-                  width={1024}
-                  height={490}
-                  alt="AI"
-                />
-
-                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
-
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                     {heroIcons.map((icon, index) => (
@@ -79,15 +69,6 @@ const Hero = () => {
             </div>
 
             <Gradient />
-          </div>
-          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-            <img
-              src={heroBackground}
-              className="w-full"
-              width={1440}
-              height={1800}
-              alt="hero"
-            />
           </div>
 
           <BackgroundCircles />
